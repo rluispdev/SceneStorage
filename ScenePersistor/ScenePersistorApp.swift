10264c2da40d2ad589b81189fffbe62ec6b52fa5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-08-04 11:57:56 -0300

    Adicionar o commit inicial

---
+++ ScenePersistor/ScenePersistorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenePersistorApp.swift
+//  ScenePersistor
+//
+//  Created by Rafael Gonzaga on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScenePersistorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
