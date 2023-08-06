65c36fe2974a2ca301f1e6b9b41db8017c73fa8a
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-08-06 16:14:37 -0300

    Creating the fill of icons and changing the color his for black.

--- ScenePersistor/ContentView.swift
+++ ScenePersistor/ContentView.swift
@@ -8,12 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isSelectd = Int()
+    
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = UIColor.black
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView (selection: $isSelectd) {
+            
+            HouseView()
+                .tabItem {
+                    if isSelectd  == 0 {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, .fill )
+                    }else {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, .none )
+                    }
+                }.tag(0)
+            
+            
+            SearchView()
+                .tabItem {
+                    if isSelectd  == 1 {
+                        Image(systemName: "magnifyingglass")
+                            .environment(\.symbolVariants, .fill )
+                    }else {
+                        Image(systemName: "magnifyingglass")
+                            .environment(\.symbolVariants, .none )
+                    }
+                }.tag(1)
+            
+            FriendsView()
+                .tabItem {
+                    if isSelectd  == 2 {
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, .fill )
+                    }else {
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, .none )
+                    }
+                }.tag(2)
+            
+            
+            BellView()
+                .tabItem {
+                    if isSelectd  == 3 {
+                        Image(systemName: "bell")
+                            .environment(\.symbolVariants, .fill )
+                    } else {
+                        Image(systemName: "bell")
+                            .environment(\.symbolVariants, .none )
+                    }
+                }.tag(3)
+            
+            InBoxView()
+                .tabItem {
+                    if isSelectd  == 4 {
+                        Image(systemName: "envelope")
+                            .environment(\.symbolVariants, .fill )
+                    } else {
+                        Image(systemName: "envelope")
+                            .environment(\.symbolVariants, .none )
+                    }
+                }.tag(4)
         }
         .padding()
     }
@@ -22,5 +83,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
+
+
---
+++ ScenePersistor/Model/ModelsStructs.swift
@@ -0,0 +1,48 @@
+//
+//  IconsViews.swift
+//  ScenePersistor
+//
+//  Created by Rafael Gonzaga on 04/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct HouseView: View {
+    var body: some View {
+        
+   
+            Text("Home")
+                .frame(width: 300, height: 500)
+        
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+struct FriendsView: View {
+var body: some View {
+        Text("Friend")
+    }
+}
+
+
+struct BellView: View {
+    var body: some View {
+        
+        Text("Notification")
+    }
+}
+
+struct InBoxView : View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+
+
